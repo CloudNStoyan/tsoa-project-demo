ffc1dcc24dba2699901e6e44efef7ba31052344a
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-08-02 10:53:25 +0300

    refactor the secure route to return the user object given by the auth controller

--- src/routes/secure.ts
+++ src/routes/secure.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Route, Security, Tags } from 'tsoa';
+import { Controller, Get, Route, Security, Tags, Request } from 'tsoa';
+import type { Request as ExpressRequest } from 'express';
 
 @Tags('Secure')
 @Route('secure')
 export class SecureController extends Controller {
   @Security('api_key', ['cat', 'dog'])
   @Get()
-  getTreasure(): Promise<string> {
-    return Promise.resolve('One Piece');
+  getTreasure(
+    @Request() request: ExpressRequest & { user: { id: number; name: string } }
+  ): Promise<{ id: number; name: string }> {
+    return Promise.resolve(request.user);
   }
 }
